2c4e38e2f71f3b789b48ab41bea6f1ae95aa8a60
Denis Dyachenko <dyachenkodennis@gmail.com>
2022-08-01 23:04:20 +0300

    Исправления_1

--- datamart_ddl.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE analysis.dm_rfm_segments (
-	user_id INT PRIMARY KEY,
-	recency smallint not null check (recency between 1 and 5),
-	frequency smallint NOT NULL CHECK (frequency between 1 and 5),
-	monetary_value smallint NOT NULL CHECK (monetary_value between 1 and 5)
-	);
--- orders_view.sql
+++
@@ -1,8 +0,0 @@
-create or replace view analysis.orders as 
-with ls as(
-select order_id, status_id, row_number () over(partition by order_id order by dttm desc) as last_status
-from production.orderstatuslog
-)
-select o.order_id, order_ts, user_id, bonus_payment, payment, cost, bonus_grant, ls.status_id as status from production.orders o
-join ls on o.order_id = ls.order_id 
-where last_status = 1;
